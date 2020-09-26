e977d91b607499a66fbc8315106631c87f85e876
u_zo <kwoojo0127@naver.com>
2020-09-26 20:12:44 +0900

    Init commit

---
+++ client/src/pages/MainPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Main from '../components/main/Main';
+import HeaderContainer from '../containers/common/HeaderContainer';
+
+const MainPage = () => {
+  return (
+      <>
+        <HeaderContainer />
+        <Main />
+      </>
+  );
+};
+
+export default MainPage;
---
+++ server/README.md
@@ -0,0 +1,1 @@
+## Class #3 Community Web Service
