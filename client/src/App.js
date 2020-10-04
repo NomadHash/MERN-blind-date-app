dcb384ff3e9dfa4a91733fa808623cd86a55e2f5
Woojo Kim <kwoojo0127@naver.com>
2020-10-04 18:55:30 +0900

    Add socket.io client module to client

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,15 @@ import { Route } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import io from 'socket.io-client';
 
 const App = () => {
+  const socket = io();
+
+  socket.on('chat', (msg) => {
+    console.log(msg);
+  });
+
   return (
     <>
       <Route component={MainPage} path="/" exact />
